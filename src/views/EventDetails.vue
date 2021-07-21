c4d09e61dd316a72acc05c130e5bc7e78e4ee42c
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-07-21 12:57:03 +0700

    Add event details and setting untill 1.10

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.data }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+
+export default {
+    props: ['id'],
+  data() {
+    return {
+      event: null,
+    //   id: 123
+    }
+  },
+   created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+}
+</script>
